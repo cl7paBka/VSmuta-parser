07a9a20a50e5523b17e2d95e3d05a29d774f6ec6
cl7paBka <maks-kandikov@yandex.ru>
2024-02-20 19:45:57 +0300

    smth

--- VSmuta-parser.py
+++ VSmuta-parser.py
@@ -4,12 +4,12 @@ import requests
 
 parser = argparse.ArgumentParser()
 parser.add_argument('Date', type=str,
-                        help='Input day in this format: YYYY-MM-DD')
+                    help='Input day in this format: YYYY-MM-DD')
 args = parser.parse_args()
 
 date = args.Date
 link = 'https://vsmuta.com/api/logs/blood/'
-response = requests.get(link+date)
+response = requests.get(link + date)
 if response.status_code == 200:
     data = json.loads(response.text)
     if data['status'] == 'success':
@@ -21,14 +21,13 @@ if response.status_code == 200:
         for battle in battles:
             for member in battle['members']:
                 clan = member['clan']
-                clans_counter.setdefault(clan, [0, 0]) #Первое значение списка - победы, второе поражения
+                clans_counter.setdefault(clan, [0, 0])  # Первое значение списка - победы, второе поражения
                 if member['team'] == 1:
                     clans_counter[clan][0] += 1
                 else:
                     clans_counter[clan][1] += 1
 
-
-        print(f"Количество игроков: {winners_counter+losers_counter}\n"
+        print(f"Количество игроков: {winners_counter + losers_counter}\n"
               f"Количество кланов: {len(clans_counter)}\n"
               f"Кланы:")
         for clan in clans_counter:
