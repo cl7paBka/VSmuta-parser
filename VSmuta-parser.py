62c6308d02503d7788c85c544310578cdc403654
cl7paBka <maks-kandikov@yandex.ru>
2024-02-26 18:35:23 +0300

    Output saving to txt added

--- VSmuta-parser.py
+++ VSmuta-parser.py
@@ -2,39 +2,72 @@ import json
 import argparse
 import requests
 
-parser = argparse.ArgumentParser()
-parser.add_argument('Date', type=str,
-                    help='Input day in this format: YYYY-MM-DD')
-args = parser.parse_args()
 
-date = args.Date
-link = 'https://vsmuta.com/api/logs/blood/'
-response = requests.get(link + date)
-if response.status_code == 200:
-    data = json.loads(response.text)
-    if data['status'] == 'success':
-        battles = data['battles']
-        winners_counter = len(battles)
-        losers_counter = len(battles)
-        clans_counter = dict()
+def parse_args():
+    global args
 
-        for battle in battles:
-            for member in battle['members']:
-                clan = member['clan']
-                clans_counter.setdefault(clan, [0, 0])  # Первое значение списка - победы, второе поражения
-                if member['team'] == 1:
-                    clans_counter[clan][0] += 1
-                else:
-                    clans_counter[clan][1] += 1
-
-        print(f"Количество игроков: {winners_counter + losers_counter}\n"
-              f"Количество кланов: {len(clans_counter)}\n"
-              f"Кланы:")
-        for clan in clans_counter:
-            print(f"{clan}\n"
-                  f"Победы: {clans_counter[clan][0]}\n"
-                  f"Поражения: {clans_counter[clan][1]}\n")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('Date', type=str,
+                        help='Input day in this format: YYYY-MM-DD')
+    parser.add_argument('-o', '--output', type=str,
+                        help='If you want to save the output to .txt file, type filepath here')
+    args = parser.parse_args()
 
 
-    else:
-        print(f"Error: {data['message']}")
+def saving_output(output_path, output):
+    if output_path is not None:
+        with open(output_path, 'w') as output_file:
+            output_file.writelines(output)
+        print(f"Output saved to {args.output}")
+
+
+def run():
+    date = args.Date
+    link = 'https://vsmuta.com/api/logs/blood/'
+    response = requests.get(link + date)
+    if response.status_code == 200:
+        data = json.loads(response.text)
+        if data['status'] == 'success':
+            battles = data['battles']
+            players_counter = 0
+            clans_counter = dict()
+
+            for battle in battles:
+                for member in battle['members']:
+                    players_counter += 1
+                    player_name = member['title']
+                    clan = member['clan']
+                    clans_counter.setdefault(clan, {'players': set(), 'wins': 0, 'losses': 0})
+                    clans_counter[clan]['players'].add(player_name)
+                    if member['team'] == 1:
+                        clans_counter[clan]['wins'] += 1
+                    else:
+                        clans_counter[clan]['losses'] += 1
+            day_information = (f"День: {date}\n"
+                               f"Количество игроков: {players_counter}\n"
+                               f"Количество кланов: {len(clans_counter)}\n"
+                               f"Кланы: \n")
+
+            for clan in clans_counter:
+                day_information += (f"{'_' * 30}\n"
+                                    f"• {clan}\n"
+                                    f"Победы: {clans_counter[clan]['wins']}\n"
+                                    f"Поражения: {clans_counter[clan]['losses']}\n"
+                                    f"Игроки: {' | '.join(name for name in clans_counter[clan]['players'])}\n"
+                                    f"{'_' * 30}\n")
+            print(day_information)
+            return day_information
+
+
+        else:
+            print(f"Error: {data['message']}")
+
+
+def main():
+    parse_args()
+    output = run()
+    saving_output(args.output, output)
+
+
+if __name__ == '__main__':
+    main()
